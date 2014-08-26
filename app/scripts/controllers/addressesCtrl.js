fa8c6f0a1422ca5b0bb67d013c0299367261a174
Sven Reglitzki <sreglitzki@googlemail.com>
2014-08-26 08:47:05 +0200

    V05 - filtering

--- app/scripts/controllers/addressesCtrl.js
+++ app/scripts/controllers/addressesCtrl.js
@@ -1,7 +1,13 @@
-angular.module("angularDemo").controller("addressesCtrl", function ($scope, addresses) {
+angular.module("angularDemo").controller("addressesCtrl", function ($scope, addresses, Addresses, $q, $timeout) {
 
     "use strict";
 
     $scope.addresses = addresses;
 
+    $scope.reload = function () {
+        // addresses = $scope.addresses = Addresses.query();
+        Addresses.query(function (addrs) {
+            addresses = $scope.addresses = addrs;
+        });
+    };
 });
